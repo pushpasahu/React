30733f0ab7fd5acd5d65e778e3cb50a7195a6a69
pushpa sahu <psahu6070@gmail.com>
2025-01-21 13:34:19 +0530

    added in main

--- first-app-final1/src/Cart.js
+++ first-app-final1/src/Cart.js
@@ -3,7 +3,7 @@ function cart() {
     return (
         <div >
             <h5>React Cart Component </h5>
-            <img src="images.jpg" alt="Italian Trulli"/>
+            <img src="images.jpg" alt="Italianddsds Trulli"/>
         </div>
       );
 }
