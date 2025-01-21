24eb0bf1436b8d5970ed0e7d239b86b980a93742
pushpa sahu <psahu6070@gmail.com>
2025-01-21 10:30:07 +0530

    changes

---
+++ first-app-final1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './Cart.css';
+import Cart from './Cart';
+function App() {
+  return (
+    <>
+      <Cart />
+    </>
+  );
+}
+
+export default App;
---
+++ first-app-final1/src/Cart.js
@@ -0,0 +1,11 @@
+
+function cart() {
+    return (
+        <div >
+            <h5>React Cart Component </h5>
+            <img src="images.jpg" alt="Italian Trulli"/>
+        </div>
+      );
+}
+
+export default cart;
